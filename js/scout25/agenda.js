8dfc760e81d19dd94f10fe5ec8b3af953975cef0
gpiron <guillaume.piron@hotmail.com>
2015-12-02 13:46:54 +0100

    Mise à jour CAL 29-11-2015 + passage au mois de décembre
    Décembre : ajout marché de noel le 13
    Janvier : ajout réunion meute 10
    Février : ajout réunion patinoire le 21 + réunions du 28
    Avril : début du camp de paques le samedi 02, fin du camp de paques castors le lundi 04

--- js/scout25/agenda.js
+++ js/scout25/agenda.js
@@ -1,22 +1,17 @@
 $( document ).ready( function() {
 	$(".responsive-calendar").responsiveCalendar({
-		time: '2015-11',
+		time: '2015-12',
 		translateMonths: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre"],
 		monthChangeAnimation: false,
 		events: {
-		  	// Novembre
-			"2015-11-21": {"number": 1, "badgeClass": "badge-warning", url: ",,,,Soirée Pionniers"},
-			"2015-11-22": {"number": 2, "badgeClass": "badge-warning", url: "normale,,normale,"},
-	  		"2015-11-27": {"number": 1, "badgeClass": "badge-warning", url: ",,,,Bar Pionniers"},
-	  		"2015-11-29": {"number": 4, "badgeClass": "badge-warning", url: "piscine,piscine,normale,normale"},
-
-	  		// Décembre
+		  	// Décembre
 	  		"2015-12-06": {"number": 2, "badgeClass": "badge-warning", url: "demi,demi,,"},
-	  		"2015-12-12": {"number": 2, "badgeClass": "badge-warning", url: ",,,speciale,Marché de Noël Ganshoren"},
-	  		"2015-12-18": {"number": 1, "badgeClass": "badge-warning", url: ",,cinéma,,"},
+	  		"2015-12-12": {"number": 2, "badgeClass": "badge-warning", url: ",,,Marché de Noël Ganshoren,Marché de Noël Ganshoren"},
+	  		"2015-12-13": {"number": 2, "badgeClass": "badge-warning", url: ",,,Marché de Noël Ganshoren,Marché de Noël Ganshoren"},
+	  		"2015-12-18": {"number": 1, "badgeClass": "badge-warning", url: ",,cheese,,"},
 
 	  		// Janvier
-	  		"2016-01-10": {"number": 2, "badgeClass": "badge-warning", url: "normale,,,normale"},
+	  		"2016-01-10": {"number": 3, "badgeClass": "badge-warning", url: "normale,normale,,normale"},
 	  		"2016-01-24": {"number": 2, "badgeClass": "badge-warning", url: ",normale,,normale"},
 	  		"2016-01-31": {"number": 4, "badgeClass": "badge-warning", url: "normale,normale,normale,normale"},
 
@@ -26,8 +21,8 @@ $( document ).ready( function() {
 	  		"2016-02-08": {"number": 1, "badgeClass": "badge-warning", url: ",,,wePi"},
 	  		"2016-02-09": {"number": 1, "badgeClass": "badge-warning", url: ",,,wePi"},
 	  		"2016-02-14": {"number": 1, "badgeClass": "badge-warning", url: ",,normale,"},
-	  		"2016-02-21": {"number": 4, "badgeClass": "badge-warning", url: "normale,normale,normale,normale"},
-	  		"2016-02-21": {"number": 3, "badgeClass": "badge-warning", url: "normale,normale,normale,"},
+	  		"2016-02-21": {"number": 4, "badgeClass": "badge-warning", url: "patinoire,patinoire,patinoire,patinoire"},
+	  		"2016-02-28": {"number": 3, "badgeClass": "badge-warning", url: "normale,normale,normale,"},
 
 	  		// Mars
 	  		"2016-03-06": {"number": 2, "badgeClass": "badge-warning", url: "normale,,normale,"},
@@ -37,17 +32,16 @@ $( document ).ready( function() {
 	  		"2016-03-26": {"number": 1, "badgeClass": "badge-warning", url: ",,,Repas Pionniers"},
 
 	  		// Avril
-	  		"2016-04-01": {"number": 4, "badgeClass": "badge-warning", url: "Camp de pâques,Camp de pâques,Camp de pâques,Camp de pâques"},
 	  		"2016-04-02": {"number": 4, "badgeClass": "badge-warning", url: "Camp de pâques,Camp de pâques,Camp de pâques,Camp de pâques"},
 	  		"2016-04-03": {"number": 4, "badgeClass": "badge-warning", url: "Camp de pâques,Camp de pâques,Camp de pâques,Camp de pâques"},
 	  		"2016-04-04": {"number": 4, "badgeClass": "badge-warning", url: "Camp de pâques,Camp de pâques,Camp de pâques,Camp de pâques"},
-	  		"2016-04-05": {"number": 4, "badgeClass": "badge-warning", url: "Camp de pâques,Camp de pâques,Camp de pâques,Camp de pâques"},
-	  		"2016-04-06": {"number": 4, "badgeClass": "badge-warning", url: "Camp de pâques,Camp de pâques,Camp de pâques,Camp de pâques"},
-	  		"2016-04-07": {"number": 4, "badgeClass": "badge-warning", url: "Camp de pâques,Camp de pâques,Camp de pâques,Camp de pâques"},
-	  		"2016-04-08": {"number": 4, "badgeClass": "badge-warning", url: "Camp de pâques,Camp de pâques,Camp de pâques,Camp de pâques"},
-	  		"2016-04-09": {"number": 4, "badgeClass": "badge-warning", url: "Camp de pâques,Camp de pâques,Camp de pâques,Camp de pâques"},
+	  		"2016-04-05": {"number": 3, "badgeClass": "badge-warning", url: ",Camp de pâques,Camp de pâques,Camp de pâques"},
+	  		"2016-04-06": {"number": 3, "badgeClass": "badge-warning", url: ",Camp de pâques,Camp de pâques,Camp de pâques"},
+	  		"2016-04-07": {"number": 3, "badgeClass": "badge-warning", url: ",Camp de pâques,Camp de pâques,Camp de pâques"},
+	  		"2016-04-08": {"number": 3, "badgeClass": "badge-warning", url: ",Camp de pâques,Camp de pâques,Camp de pâques"},
+	  		"2016-04-09": {"number": 3, "badgeClass": "badge-warning", url: ",Camp de pâques,Camp de pâques,Camp de pâques"},
 	  		"2016-04-17": {"number": 4, "badgeClass": "badge-warning", url: "normale,normale,normale,normale"},
-	  		"2016-04-17": {"number": 1, "badgeClass": "badge-warning", url: ",,,,Fête d'Unité"},
+	  		"2016-04-23": {"number": 1, "badgeClass": "badge-warning", url: ",,,,Fête d'Unité"},
 
 	  		// Mai
 	  		"2016-05-01": {"number": 4, "badgeClass": "badge-warning", url: "normale,normale,normale,normale"},
@@ -63,8 +57,9 @@ $( document ).ready( function() {
 	var calendarModal = $("#calendarModal");
 	var modalContent = $("#modalContent");
 	var reunion = {
-		    cinema: "Soirée Cheese and Movies, infos à suivre",
-		    piscine: "Réunion piscine : prévoir maillot, bonnet, essuie, (lunettes) et 5,00€",
+		    	patinoire: "Réunion patinoire, infos suivront",
+		        cheese: "Soirée Cheese and Movies, infos à suivre",
+		        piscine: "Réunion piscine : prévoir maillot, bonnet, essuie, (lunettes) et 5,00€",
 			normale: "Réunion normale",
 			speciale: "Réunion spéciale",
 			demi: "Demi Réunion, de 14h à 17h.",
@@ -102,12 +97,14 @@ $( document ).ready( function() {
 				thisReunion = reunion.normale;
 			else if (reunions[i] == "speciale")
 				thisReunion = reunion.speciale;
-		    else if (reunions[i] == "demi")
+			else if (reunions[i] == "demi")
 				thisReunion = reunion.demi;
-		    else if (reunions[i] == "cinéma")
-				thisReunion = reunion.cinema;
-		    else if (reunions[i] == "piscine")
+		    else if (reunions[i] == "cheese")
+				thisReunion = reunion.cheese;
+			else if (reunions[i] == "piscine")
 				thisReunion = reunion.piscine;
+			else if (reunions[i] == "patinoire")
+				thisReunion = reunion.patinoire;
 			else if (reunions[i] == "wePi")
 				thisReunion = reunion.wePi;
 			else if (reunions[i] == "velo")
